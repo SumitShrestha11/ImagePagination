e65d49a25a1304b5e4f11be12a2b627151582a05
Sumit <sumit.sth54@gmail.com>
2024-11-19 21:47:36 +0545

    chore: added background gradient

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,12 @@ import ImageSearch from "./components/custom/ImageSearch";
 function App() {
   return (
     <>
-      {/* <div className="relative w-full h-fit bg-black/80"> */}
-      {/* <div className="absolute inset-0 bg-black/50"> */}
-      {/* <AnimatedGridPattern
-            numSquares={30}
-            maxOpacity={0.1}
-            duration={3}
-            repeatDelay={1}
-            className={cn(
-              "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
-              "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12"
-            )}
-          /> */}
-      {/* </div>
-        <div className="relative z-10"> */}
-      <ImageSearch />
-      {/* </div>
-      </div> */}
+      <div className="relative w-full h-fit bg-black/80">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-100 via-blue-50 to-gray-200" />
+        <div className="relative z-10">
+          <ImageSearch />
+        </div>
+      </div>
     </>
   );
 }
